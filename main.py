48f2d9adf3862d467e3f011b1fa88b12c476ca2f
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-09-01 21:53:58 +0200

    Minor improvements

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ MAC_WIDTH = 1440
 MAC_HEIGHT = 900
 FPS = 30
 FRAME_SIZE = tuple((MAC_WIDTH * 2, MAC_HEIGHT * 2))
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, FPS, (FRAME_SIZE))
-monitor = {"top": 0, "left": 0, "width": 1440, "height": 900}
 
-with mss(with_cursor=True) as sct:
-    for _ in range(FPS * 10):
-        img = np.array(sct.grab(monitor))
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        out.write(img)
-out.release()
+def main():
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output.mp4', fourcc, FPS, (FRAME_SIZE))
+    monitor = {"top": 0, "left": 0, "width": MAC_WIDTH, "height": MAC_HEIGHT}
+    with mss(with_cursor=True) as sct:
+        for _ in range(FPS * 10):
+            img = np.array(sct.grab(monitor))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            out.write(img)
+    out.release()
+
+if __name__ == "__main__":
+    main()
