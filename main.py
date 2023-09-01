0587f71931c46364b6bbd7393742ee15049cb0e7
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-09-02 00:24:54 +0200

    Build fully working example with already created blocks

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-import numpy as np
-import cv2
-from mss import mss
+from screen_recorder import record
+from recognize_speech import recognize_speech
 
-MAC_WIDTH = 1440
-MAC_HEIGHT = 900
-FPS = 30
-FRAME_SIZE = tuple((MAC_WIDTH * 2, MAC_HEIGHT * 2))
 
 def main():
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('output.mp4', fourcc, FPS, (FRAME_SIZE))
-    monitor = {"top": 0, "left": 0, "width": MAC_WIDTH, "height": MAC_HEIGHT}
-    with mss(with_cursor=True) as sct:
-        for _ in range(FPS * 10):
-            img = np.array(sct.grab(monitor))
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            out.write(img)
-    out.release()
+    """
+    If 'clip it' said records screen for 10 seconds and saves it to output.mp4
+    """
+    while True:
+        text = recognize_speech()
+        if not text == 'clip it':
+            continue
+        return record()
 
 if __name__ == "__main__":
     main()
